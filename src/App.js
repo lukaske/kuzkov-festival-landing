3b51ae67b3ae62eeb0241855334affebaf5b97ff
Luka Skeledzija <36110818+lukaske@users.noreply.github.com>
2020-12-09 18:40:50 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <img
+        href="https://kuzek.si"
+        style={{ width: "100%" }}
+        src={
+          "https://festivaldrop.s3.eu-north-1.amazonaws.com/dogodek-2020-cove-1920x1080+(1).jpg"
+        }
+      />
+      <h2 style={{ color: "#189adf" }}>
+        Do takrat nas lahko obiščete na{" "}
+        <a style={{ color: "#189adf" }} href="https://kuzek.si">
+          kuzek.si
+        </a>
+      </h2>
+    </div>
+  );
+}
