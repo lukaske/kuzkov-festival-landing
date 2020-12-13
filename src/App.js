9b42c98ca7ad7b4241b367c8fa42fed4dba7e6c9
Luka Skeledzija <36110818+lukaske@users.noreply.github.com>
2020-12-14 00:10:10 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+  var p = {"Kotmezika za male živali Arava":"https://arava.si/","Krematorij za male živali Cremona":"http://www.cremona.si/sl/","Društvi CANIS - REPS":"https://www.slo-canis.net/","Trgovina Furry Care":"https://furry-care.com/","Hotel za pse Dog Holiday":"http://dog-holiday.si/","Unikatni izdelki Karoks":"https://unikatnoizsodov.si/","Revija Kužek":"https://kuzek.si/","Mačkarna Karieirian - devon rex":"Mačkarna Karieirian - devon rex","Trgovina z oblačili Pancho":"https://www.pancho.si/","Pasji salon Kr tačko":"https://www.facebook.com/Pasji-salon-KrTa%C4%8Dko-108839584268549/","Trgovina Petsi":"https://petsi.eu/","Šola za pse Planet K9":"https://planetk9.si/","Trgovina Prestige Pet":"https://www.prestige-pet.si/","Psarna Myn Senca":"https://mynsenca.com/Saarloos/","Hrana za živali Purina":"https://www.arc.si/sl/purina-one","Društvo S hrti za hrte":"https://shrtizahrte.wixsite.com/shrtizahrte","Trgovina Buba":"https://www.buba-trgovina.si/","Revija Mucek":"https://mucek.si"}
+
   return (
     <div className="App">
       <img
@@ -13,11 +15,18 @@ export default function App() {
         }
       />
       <h2 style={{ color: "#189adf" }}>
-        Do takrat nas lahko obiščete na{" "}
+        Kužkov Festival 2020 je zaključen. Hvala za udeležbo!
+        <br/>
         <a style={{ color: "#189adf" }} href="https://kuzek.si">
-          kuzek.si
+          Se vidimo na kuzek.si
         </a>
       </h2>
+      <ul className="sodelujoci">
+        {Object.keys(p).forEach(function (key) {
+            if (p.hasOwnProperty(key)) {
+              return <li><a href={p[key]}>{key}</a></li>
+              }})}
+      </ul>
     </div>
   );
 }
